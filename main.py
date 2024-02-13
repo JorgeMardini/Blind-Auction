aca9750ba4b98f9b32c34885c272ac589910f87a
JorgeMardini <159563060+JorgeMardini@users.noreply.github.com>
2024-02-13 13:16:31 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from replit import clear
+#HINT: You can call clear() to clear the output in the console.
+from art import logo
+print(logo)
+
+bids = {}
+bidding_finished = False
+
+def find_highest_bidder(bidding_record):
+  highest_bid = 0
+  for bidder in bidding_record:
+    bid_amount = bidding_record[bidder]
+    if bid_amount > highest_bid:
+      highest_bid = bid_amount
+  print(f"The winner is {name} with a bid of £{highest_bid}.")
+
+while not bidding_finished:
+  name = input("Enter your name: ")
+  bid = int(input("Enter your bid: £"))
+  bids[name] = bid
+  should_continue = input("Are there any other bidders? Type 'yes' or 'no'.")
+  if should_continue == "no":
+    bidding_finished = True
+    find_highest_bidder(bids)
+  elif should_continue == "yes":
+    clear()
+
